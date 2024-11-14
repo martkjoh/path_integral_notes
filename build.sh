0af3f62715b60e392b14412c0c9bcccd98df22c3
Martin Johnsrud <martin.johnsrud@ds.mpg.de>
2024-11-14 21:06:41 +0100

    publish

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 # For gihub actions
 
-latexmk main.tex
+pdflatex -interaction=nonstopmode --output-directory=build/ main.tex
+pdflatex -interaction=nonstopmode --output-directory=build/ main.tex
 
-# pdflatex -interaction=nonstopmode --output-directory=build/ main.tex
-# pdflatex -interaction=nonstopmode --output-directory=build/ main.tex
+cd build/
+mpost feyn.mp
+cd ../
 
-# cd build/
-# mpost feyn.mp
-# cd ../
-
-# pdflatex -interaction=nonstopmode --output-directory=build/ main.tex
+pdflatex -interaction=nonstopmode --output-directory=build/ main.tex
